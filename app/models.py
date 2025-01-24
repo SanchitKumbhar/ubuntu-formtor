2c0cc19c4728e1c2b33e5d3c0d9c8edee5d277c8
Sanchit Kumbhar <sanchitskumbhar@gmail.com>
2025-01-24 07:36:27 +0530

    changes

--- app/models.py
+++ app/models.py
@@ -37,6 +37,7 @@ class FormData(models.Model):
     user = models.ForeignKey(CustomUser, name="customuser", on_delete=models.CASCADE)
     event = models.ForeignKey(FormInfo, name="event", on_delete=models.CASCADE)
     timestamp = models.DateField(null=True)
+    detect=models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.data)
